705b8be84f0e70c80054610d7da6a75f59e77672
Roc Boronat <roc@fewlaps.com>
2019-04-20 16:36:25 -0500

    Create a review.php to make it easier to invite people to review the app in places like emails

---
+++ review.php
@@ -0,0 +1,18 @@
+<?php
+if ( isset( $_SERVER['HTTP_USER_AGENT'] ) ) {
+	$iOS = stripos( $_SERVER['HTTP_USER_AGENT'], "iOS" );
+	$iPod = stripos( $_SERVER['HTTP_USER_AGENT'], "iPod" );
+	$iPhone = stripos( $_SERVER['HTTP_USER_AGENT'], "iPhone" );
+	$iPad = stripos( $_SERVER['HTTP_USER_AGENT'], "iPad" );
+	$Android = stripos( $_SERVER['HTTP_USER_AGENT'], "Android" );
+}
+if ( $iOS || $iPod || $iPhone || $iPad ) {
+	header( 'Location: https://itunes.apple.com/app/quitnow!-quit-smoking/id483994930' );
+	die();
+} else if ( $Android ) {
+	header( 'Location: https://play.google.com/store/apps/details?id=com.EAGINsoftware.dejaloYa&referrer=utm_source%3Dreview_redirect' );
+	die();
+} else { 
+	header( 'Location: http://quitnowapp.com#Downloads' );
+}
+?>
