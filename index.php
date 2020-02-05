6b97dea5008f1dc1a2264653419b9f7b0e1a4af1
Roc Boronat <roc@fewlaps.com>
2020-02-05 07:51:09 +0100

    Add the Facebook SDK to the website

--- index.php
+++ index.php
@@ -190,6 +190,25 @@ function gtag_conversion(url) {
 </head>
 <body>
 
+<script>
+  window.fbAsyncInit = function() {
+    FB.init({
+      appId      : '156729571066410',
+      xfbml      : true,
+      version    : 'v6.0'
+    });
+    FB.AppEvents.logPageView();
+  };
+
+  (function(d, s, id){
+     var js, fjs = d.getElementsByTagName(s)[0];
+     if (d.getElementById(id)) {return;}
+     js = d.createElement(s); js.id = id;
+     js.src = "https://connect.facebook.net/en_US/sdk.js";
+     fjs.parentNode.insertBefore(js, fjs);
+   }(document, 'script', 'facebook-jssdk'));
+</script>
+    
 <header>
 <img src="images/Quitnow.svg" class="logo" alt="QuitNow!">
 <span class="stores">
